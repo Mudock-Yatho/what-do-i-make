54d22413de04dfa9cbc455c5096f17dc40a7005c
Mudock R Yatho <MudockYatho@Gmail.com>
2018-03-27 08:51:13 -0500

    Update skree.cpp

--- skree.cpp
+++ skree.cpp
@@ -1,3 +1,15 @@
-int main(){
+#include <iostream>
+void test(){
+	for(int a=0;a<5;a++){
+		std::cout<<"test"<<std::endl;
+	}
+}
+int main (){
+	int a=1;
+	float b=299.5;
+	std::cout<<"testing\n";
+	std::cout<<"testx2\n";
+	std::cout<<a+b<<std::endl;
+	test();
 	return 0;
 }
