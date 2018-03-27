8275b7b0fff7175d27612c9dbf0445e2d2745b9a
Mudock R Yatho <MudockYatho@Gmail.com>
2018-03-27 10:15:09 -0500

    update
    uhhh add more functions

--- skree.cpp
+++ skree.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 void test(){
 	for(int a=0;a<5;a++){
-		std::cout<<"test"<<std::endl;
+		if(a==4){
+			std::cout<<"test";
+		}else{
+			std::cout<<"test\n";
+		}
 	}
+}
+int test2(){
+	for(int a=0;a<3;a++){
+		std::cout<<".";
+	}
+	return 0;
 }
 int main (){
 	int a=1;
@@ -11,5 +21,6 @@ int main (){
 	std::cout<<"testx2\n";
 	std::cout<<a+b<<std::endl;
 	test();
+	test2();
 	return 0;
 }
