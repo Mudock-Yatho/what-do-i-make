4cc1a457ed68d734b222a5b39d56d7285d2cf450
Mudock R Yatho <MudockYatho@Gmail.com>
2019-06-18 21:18:04 -0500

    Update skree.cpp

--- skree.cpp
+++ skree.cpp
@@ -1,26 +1,46 @@
+/*
+	This code is evil.
+*/
+#include "pch.h"
 #include <iostream>
-void test(){
-	for(int a=0;a<5;a++){
-		if(a==4){
-			std::cout<<"testing finished";
-		}else{
-			std::cout<<"test\n";
+#include <chrono>
+#include <Windows.h>
+#include <thread>
+#include <vector>
+void Output(int Core, int ins) {
+	for (int count = 1; count < ins + 1; count++) {
+		std::cout << "[ " << Core << " ] " << count << "\n";
+	}
+	return;
+}
+int main() {
+	SetConsoleTitle(L"Penis");
+	const unsigned int Cores = std::thread::hardware_concurrency();
+	std::cout << "Cores detected: " << Cores << "\n";
+	std::cout << "How many instructions: ";
+	int ins;
+	while (!(std::cin >> ins) || ins <= 0) {
+		std::cout << "Please enter a positive number above 0 for the instructions amount.\nHow many instructions: ";
+		std::cin.clear();
+		std::cin.ignore(1000, '\n');
+	}
+	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+	if (ins + 1 > Cores) {
+		const int InstructionPerCore = (ins) / Cores;
+		std::cout << "Instructions for each core: " << InstructionPerCore << "\n";
+		std::vector<std::thread> threads(Cores);
+		for (int Core = 0; Core < Cores; Core++) {
+			threads[Core] = std::thread(Output, Core + 1, InstructionPerCore);
 		}
+		for (int Core = 0; Core < Cores; Core++) {
+			threads[Core].join();
+		}
+		std::cout << "Took: " << std::chrono::duration_cast<std::chrono::seconds> (std::chrono::steady_clock::now() - begin).count() << "s using " << Cores << " threads.\n";
+	} else {
+		std::cout << "Using single-thread mode.\n";
+		Output(1, ins);
+		std::cout << "Took: " << std::chrono::duration_cast<std::chrono::seconds> (std::chrono::steady_clock::now() - begin).count() << "s using single-thread.\n";
 	}
-}
-int test2(){
-	for(int a=0;a<3;a++){
-		std::cout<<".";
-	}
-	return 0;
-}
-int main (){
-	int a=1;
-	float b=299.5;
-	std::cout<<"testing\n";
-	std::cout<<"test x2\n";
-	std::cout<<a+b<<"\n";
-	test();
-	test2();
+	system("pause");
 	return 0;
 }
